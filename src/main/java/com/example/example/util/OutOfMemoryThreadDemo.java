3a92c5a2a8c11e28a299d2f656d25187d743f022
anhyeongkwan <bombom841@naver.com>
2023-12-02 20:13:46 +0900

    - out of memory test2

--- src/main/java/com/example/example/util/OutOfMemoryThreadDemo.java
+++ src/main/java/com/example/example/util/OutOfMemoryThreadDemo.java
@@ -1,5 +1,7 @@
 package com.example.example.util;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class OutOfMemoryThreadDemo {
@@ -11,13 +13,17 @@ public class OutOfMemoryThreadDemo {
             try {
                 i++;
                 list.add(new byte[1000000]);
-                System.out.println(i);
+                System.out.println("Added " + i + "MB");
                 Thread.sleep(1);
+            } catch (OutOfMemoryError error) {
+                System.out.println("out of memory : " + i + "MB");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
     }
+
+
 }
 
 class Worker implements Runnable{
